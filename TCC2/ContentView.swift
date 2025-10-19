d708d876c8b3cfa18723c6588f07f5e61d31f5ce
Sonzzini <paulo.sonzzini@gmail.com>
2025-10-19 18:39:04 -0300

    Initial Commit

---
+++ TCC2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TCC2
+//
+//  Created by Paulo Sonzzini Ribeiro de Souza on 19/10/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TCC2/TCC2App.swift
@@ -0,0 +1,17 @@
+//
+//  TCC2App.swift
+//  TCC2
+//
+//  Created by Paulo Sonzzini Ribeiro de Souza on 19/10/25.
+//
+
+import SwiftUI
+
+@main
+struct TCC2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
